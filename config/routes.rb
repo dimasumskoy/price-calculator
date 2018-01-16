30467ccc2d37c22898d5336bf3e6dcf88d61a610
Dima <sumskoydima@gmail.com>
2018-01-17 01:41:09 +0300

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/prices/calculate', to: 'prices#new'
-  get '/prices', to: 'prices#calculate'
+  get '/price/calculate', to: 'prices#new'
+  get '/price', to: 'prices#calculate'
 end
