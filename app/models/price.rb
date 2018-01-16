f4b1a4a36119bb090eb12a07c710f809d1079e4c
Dima <sumskoydima@gmail.com>
2018-01-16 23:32:09 +0300

    Refactor Price.calculate

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,11 +1,11 @@
 class Price < ApplicationRecord
-  def self.calculate(params)
-    depart_city   = params[:cities][:depart_city]
-    delivery_city = params[:cities][:delivery_city]
-    weight        = params[:weight].to_i
-    length        = params[:length].to_i
-    width         = params[:width].to_i
-    height        = params[:height].to_i
+  def self.calculate(price_params)
+    depart_city   = price_params[:cities][:depart_city]
+    delivery_city = price_params[:cities][:delivery_city]
+    weight        = price_params[:weight].to_i
+    length        = price_params[:length].to_i
+    width         = price_params[:width].to_i
+    height        = price_params[:height].to_i
 
     max_area_id    = City.max_area(depart_city, delivery_city)
     max_area_price = Area.find(max_area_id).price
